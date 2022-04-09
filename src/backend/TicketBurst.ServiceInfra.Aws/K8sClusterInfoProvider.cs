06844e328e36ddf00b437007302dfe2f3960a8e7
felixbrm <felixbrm@amazon.com>
2022-04-09 12:13:39 +0300

    Reservation service - still trying to get statefulset description from K8s api

--- src/backend/TicketBurst.ServiceInfra.Aws/K8sClusterInfoProvider.cs
+++ src/backend/TicketBurst.ServiceInfra.Aws/K8sClusterInfoProvider.cs
@@ -21,41 +21,86 @@ public class K8sClusterInfoProvider : IClusterInfoProvider
             var config = KubernetesClientConfiguration.BuildDefaultConfig();
             _client = new Kubernetes(config);
             
-            Console.WriteLine("K8sClusterInfoProvider: initializing 1");
+            Console.WriteLine("K8sClusterInfoProvider: initializing 0");
             Console.WriteLine($"K8sClusterInfoProvider: this pod name [{Environment.MachineName}]");
 
-            var statefulSetList = _client.ListNamespacedStatefulSet(
-                namespaceParameter: _namespaceName,
-                labelSelector: $"appsvc={_serviceName}");
-            
-            Console.WriteLine($"K8sClusterInfoProvider: ListNamespacedStatefulSet returned {statefulSetList.Items.Count} items");
-
-            foreach (var statefulSet in statefulSetList.Items)
+            try
             {
-                Console.WriteLine("---item---");
-                Console.WriteLine($"statefulSet.Spec.Replicas={statefulSet.Spec.Replicas}");
-                Console.WriteLine($"statefulSet.Metadata.ClusterName={statefulSet.Metadata.ClusterName}");
-                Console.WriteLine($"statefulSet.Metadata.Name={statefulSet.Metadata.Name}");
-                Console.WriteLine($"statefulSet.Metadata.Generation={statefulSet.Metadata.Generation}");
-                Console.WriteLine($"statefulSet.Status.ObservedGeneration={statefulSet.Status.ObservedGeneration}");
-                Console.WriteLine($"statefulSet.Status.Replicas={statefulSet.Status.Replicas}");
-                Console.WriteLine($"statefulSet.Status.CurrentRevision={statefulSet.Status.CurrentRevision}");
-                Console.WriteLine($"statefulSet.Status.CurrentReplicas{statefulSet.Status.CurrentReplicas}");
-                Console.WriteLine($"statefulSet.Status.AvailableReplicas={statefulSet.Status.AvailableReplicas}");
+                var statefulSetName = $"{_serviceName}-statefulset";
+                Console.WriteLine($"Retrieving specific stateful set [{statefulSetName}]");
+                
+                var specificStatefulSet = _client.ReadNamespacedStatefulSet(statefulSetName, _namespaceName);
+                Console.WriteLine($"Specific stateful set retrieved? {(specificStatefulSet != null ? "YES" : "NO")}");
+                if (specificStatefulSet != null)
+                {
+                    PrintStatefulSet(specificStatefulSet);
+                    Current = CreateInfo(specificStatefulSet);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
             }
 
-            if (statefulSetList.Items.Count > 0)
+            Console.WriteLine("K8sClusterInfoProvider: initializing 1");
+            
+            try
             {
-                var statefulSet = statefulSetList.Items[0];
-                var memberCount = statefulSet.Spec.Replicas.GetValueOrDefault(-1); 
-                Current = new ClusterInfo(
-                    MemberCount: memberCount,
-                    ThisMemberIndex: -1,
-                    MemberHostNames: Enumerable.Range(0, memberCount)
-                        .Select(i => $"{_serviceName}-{i}")
-                        .ToImmutableList(),
-                    Generation: statefulSet.Metadata.Generation.GetValueOrDefault(-1)
-                );
+                var labelSelector = $"appsvc={_serviceName}";
+                Console.WriteLine($"Listing namespaced stateful sets with label selector [{labelSelector}]");
+                var statefulSetListByLabels = _client.ListNamespacedStatefulSet(
+                    namespaceParameter: _namespaceName,
+                    labelSelector: labelSelector);
+                if (statefulSetListByLabels != null)
+                {
+                    Console.WriteLine($"ListNamespacedStatefulSet returned {statefulSetListByLabels.Items.Count} items");
+                    foreach (var statefulSet in statefulSetListByLabels.Items)
+                    {
+                        PrintStatefulSet(statefulSet);
+                    }
+
+                    if (statefulSetListByLabels.Items.Count > 0)
+                    {
+                        Current = CreateInfo(statefulSetListByLabels.Items[0]);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"ListNamespacedStatefulSet returned NULL");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.WriteLine("K8sClusterInfoProvider: initializing 2");
+
+            try
+            {
+                Console.WriteLine($"Listing namespaced stateful sets without label selectors");
+                var statefulSetListNoLabels = _client.ListNamespacedStatefulSet(namespaceParameter: _namespaceName);
+                if (statefulSetListNoLabels != null)
+                {
+                    Console.WriteLine(
+                        $"ListNamespacedStatefulSet returned {statefulSetListNoLabels.Items.Count} items");
+                    foreach (var statefulSet in statefulSetListNoLabels.Items)
+                    {
+                        PrintStatefulSet(statefulSet);
+                    }
+                    if (statefulSetListNoLabels.Items.Count > 0)
+                    {
+                        Current = CreateInfo(statefulSetListNoLabels.Items[0]);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"ListNamespacedStatefulSet returned NULL");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
             }
 
             Console.WriteLine("K8sClusterInfoProvider: initializing 3");
@@ -69,6 +114,34 @@ public class K8sClusterInfoProvider : IClusterInfoProvider
     public ClusterInfo Current { get; } = ClusterInfo.Empty;
     
     public event Action? Changed;
+
+    private void PrintStatefulSet(V1StatefulSet statefulSet)
+    {
+        Console.WriteLine($"------statefulset[{statefulSet.Namespace()}/{statefulSet.Name()}]------");
+        Console.WriteLine($"statefulSet.Spec.Replicas={statefulSet.Spec.Replicas}");
+        Console.WriteLine($"statefulSet.Metadata.ClusterName={statefulSet.Metadata.ClusterName}");
+        Console.WriteLine($"statefulSet.Metadata.Name={statefulSet.Metadata.Name}");
+        Console.WriteLine($"statefulSet.Metadata.Generation={statefulSet.Metadata.Generation}");
+        Console.WriteLine($"statefulSet.Status.ObservedGeneration={statefulSet.Status.ObservedGeneration}");
+        Console.WriteLine($"statefulSet.Status.Replicas={statefulSet.Status.Replicas}");
+        Console.WriteLine($"statefulSet.Status.CurrentRevision={statefulSet.Status.CurrentRevision}");
+        Console.WriteLine($"statefulSet.Status.CurrentReplicas{statefulSet.Status.CurrentReplicas}");
+        Console.WriteLine($"statefulSet.Status.AvailableReplicas={statefulSet.Status.AvailableReplicas}");
+    }
+    
+    private ClusterInfo CreateInfo(V1StatefulSet statefulSet)
+    {
+        var memberCount = statefulSet.Spec.Replicas.GetValueOrDefault(-1); 
+
+        return new ClusterInfo(
+            MemberCount: memberCount,
+            ThisMemberIndex: -1,
+            MemberHostNames: Enumerable.Range(0, memberCount)
+                .Select(i => $"{statefulSet.Name()}-{i}")
+                .ToImmutableList(),
+            Generation: statefulSet.Metadata.Generation.GetValueOrDefault(-1)
+        );
+    }
 
     private ClusterInfo CreateInfo(V1PodList pods)
     {
